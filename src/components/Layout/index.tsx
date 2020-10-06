cfa5499f3da5d18669a925a0d1c154dc9c3fca92
MarcosVel <marcos.veloso@sempreceub.com>
2020-10-05 23:52:41 -0300

    UserList finalizado

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,6 +7,7 @@ import ServerName from '../ServerName';
 import ChannelInfo from '../ChannelInfo';
 import ChannelList from '../ChannelList';
 import UserInfo from '../UserInfo';
+import UserList from '../UserList';
 
 const Layout: React.FC = () => {
    return (
@@ -15,7 +16,9 @@ const Layout: React.FC = () => {
          <ServerName />
          <ChannelInfo />
          <ChannelList />      
-         <UserInfo />      
+         <UserInfo />
+         <div />
+         <UserList />      
       </Grid>
    );
 };
