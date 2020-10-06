c516f19e4b40fb07de6d48191e1d7113ed902440
MarcosVel <marcos.veloso@sempreceub.com>
2020-10-06 00:47:53 -0300

    ChannelData inserido com Input

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+   return (
+      <Container>
+         <Messages />
+
+         <InputWrapper>
+            <Input type='text' placeholder='Conversar em #chat-livre' />
+            <InputIcon />            
+         </InputWrapper>
+      </Container>
+   );
+}
+
+export default ChannelData;
